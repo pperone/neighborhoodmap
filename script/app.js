6ec8eb25555a74a08b1157395e955ec2369b38bb
pperone <pperone@gmail.com>
2018-08-29 14:23:02 -0300

    Corrections in responsiveness and API

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
-// Global Variables
 var map;
-var markers = [];
+var infoWindow;
+var bounds;
 var locations = [
   {title: 'Red Robin', location: {lat: 47.750197, lng: -117.3991093}},
   {title: 'Five Guys', location: {lat: 47.7443494, lng: -117.4060594}},
@@ -9,321 +9,344 @@ var locations = [
   {title: 'Pizza Hut', location: {lat: 47.7499196, lng: -117.4006212}}
 ];
 
-function AppViewModel() {
-  var self = this;
-
-  this.searched = ko.observable('');
-  this.locationsArray = ko.observableArray([]);
-
-  self.filteredList = ko.computed(function() {
-    return ko.utils.arrayFilter(locations, function(location) {
-      return location.title.toLowerCase().indexOf(self.searched().toLowerCase()) !== -1;
-    });
-  }, self);
-
-  this.showLocation = function(clicked) {
-    map.panTo(new google.maps.LatLng(clicked.location));
-  };
-};
-
 function initMap() {
-  var styles = [
-    {
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "hue": "#ff4400"
-            },
-            {
-                "saturation": -68
-            },
-            {
-                "lightness": -4
-            },
-            {
-                "gamma": 0.72
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.icon"
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "hue": "#0077ff"
-            },
-            {
-                "gamma": 3.1
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "hue": "#00ccff"
-            },
-            {
-                "gamma": 0.44
-            },
-            {
-                "saturation": -33
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "stylers": [
-            {
-                "hue": "#44ff00"
-            },
-            {
-                "saturation": -23
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "hue": "#007fff"
-            },
-            {
-                "gamma": 0.77
-            },
-            {
-                "saturation": 65
-            },
-            {
-                "lightness": 99
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "gamma": 0.11
-            },
-            {
-                "weight": 5.6
-            },
-            {
-                "saturation": 99
-            },
-            {
-                "hue": "#0091ff"
-            },
-            {
-                "lightness": -86
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": -48
-            },
-            {
-                "hue": "#ff5e00"
-            },
-            {
-                "gamma": 1.2
-            },
-            {
-                "saturation": -23
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "saturation": -64
-            },
-            {
-                "hue": "#ff9100"
-            },
-            {
-                "lightness": 16
-            },
-            {
-                "gamma": 0.47
-            },
-            {
-                "weight": 2.7
-            }
-        ]
-    }
-  ];
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.7476497, lng: -117.4048635},
-    zoom: 15.8,
-    styles: styles,
-    mapTypeControl: false
-  });
+    var styles = [
+      {
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ff4400"
+              },
+              {
+                  "saturation": -68
+              },
+              {
+                  "lightness": -4
+              },
+              {
+                  "gamma": 0.72
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.icon"
+      },
+      {
+          "featureType": "landscape.man_made",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#0077ff"
+              },
+              {
+                  "gamma": 3.1
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "stylers": [
+              {
+                  "hue": "#00ccff"
+              },
+              {
+                  "gamma": 0.44
+              },
+              {
+                  "saturation": -33
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "stylers": [
+              {
+                  "hue": "#44ff00"
+              },
+              {
+                  "saturation": -23
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "hue": "#007fff"
+              },
+              {
+                  "gamma": 0.77
+              },
+              {
+                  "saturation": 65
+              },
+              {
+                  "lightness": 99
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "gamma": 0.11
+              },
+              {
+                  "weight": 5.6
+              },
+              {
+                  "saturation": 99
+              },
+              {
+                  "hue": "#0091ff"
+              },
+              {
+                  "lightness": -86
+              }
+          ]
+      },
+      {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": -48
+              },
+              {
+                  "hue": "#ff5e00"
+              },
+              {
+                  "gamma": 1.2
+              },
+              {
+                  "saturation": -23
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "saturation": -64
+              },
+              {
+                  "hue": "#ff9100"
+              },
+              {
+                  "lightness": 16
+              },
+              {
+                  "gamma": 0.47
+              },
+              {
+                  "weight": 2.7
+              }
+          ]
+      }
+    ];
 
-  var largeInfowindow = new google.maps.InfoWindow();
-  var defaultIcon = makeMarkerIcon();
-  var highlightedIcon = makeMarkerIconHighlight();
-
-  for (var i = 0; i < locations.length; i++) {
-
-    var position = locations[i].location;
-    var title = locations[i].title;
-
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      icon: defaultIcon,
-      id: i
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 47.7476497, lng: -117.4048635},
+      zoom: 15.8,
+      styles: styles,
+      mapTypeControl: false
     });
 
-    markers.push(marker);
+    infoWindow = new google.maps.InfoWindow();
 
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-      reCenterMap(this.position);
+    bounds = new google.maps.LatLngBounds();
+
+    ko.applyBindings(new ViewModel());
+}
+
+// handle map error
+function googleMapsError() {
+    alert('An error occurred with Google Maps.');
+}
+
+/* Location Model */
+var LocationMarker = function(locations) {
+    var self = this;
+
+    this.title = locations.title;
+    this.position = locations.location;
+    this.street = '',
+    this.city = '',
+    this.phone = '';
+
+    this.visible = ko.observable(true);
+
+    var defaultIcon = makeMarkerIcon();
+    var highlightedIcon = makeMarkerIconHighlight();
+
+    // FourSquare info
+    var clientID = 'ILHXHJ5VB1R2U55VV5WPHCEZDY5KPTLJQO1ZB3CUSWADPPT2';
+    var clientSecret = 'EHB0HMPWVBDVKCRVBFHB3OHBJQY0CPEBFZ3KO3LZFZTFJX1L';
+    var reqURL = 'https://api.foursquare.com/v2/venues/search?ll=' + this.position.lat + ',' + this.position.lng + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.title;
+
+    $.getJSON(reqURL).done(function(locations) {
+		var results = locations.response.venues[0];
+        self.street = results.location.formattedAddress[0] ? results.location.formattedAddress[0]: 'N/A';
+        self.city = results.location.formattedAddress[1] ? results.location.formattedAddress[1]: 'N/A';
+        self.phone = results.contact.formattedPhone ? results.contact.formattedPhone : 'N/A';
+    }).fail(function() {
+        alert('An error occurred with FourSquare.');
     });
-    marker.addListener('mouseover', function() {
-      this.setIcon(highlightedIcon);
+
+    // Create a marker per location, and put into markers array
+    this.marker = new google.maps.Marker({
+        position: this.position,
+        title: this.title,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon
     });
-    marker.addListener('mouseout', function() {
-      this.setIcon(defaultIcon);
-    });
-    marker.reCenterMap = function (position) {
-  		map.panTo(new google.maps.LatLng(position));
-  	};
-  }
 
-  function showListings() {
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-      bounds.extend(markers[i].position);
-    }
-    map.fitBounds(bounds);
-  }
-
-  function hideListings() {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-  }
-
-  function populateInfoWindow(marker, infowindow) {
-
-      if (infowindow.marker != marker) {
-
-        infowindow.setContent('');
-        infowindow.marker = marker;
-
-        infowindow.addListener('closeclick', function() {
-          infowindow.marker = null;
-        });
-
-        var streetViewService = new google.maps.StreetViewService();
-        var radius = 50;
-
-        function getStreetView(data, status) {
-          if (status == google.maps.StreetViewStatus.OK) {
-            var nearStreetViewLocation = data.location.latLng;
-            var heading = google.maps.geometry.spherical.computeHeading(
-              nearStreetViewLocation, marker.position);
-              infowindow.setContent('<br><div id="window-title">' + marker.title + '</div><br><div id="pano"></div><br>');
-              var panoramaOptions = {
-                position: nearStreetViewLocation,
-                pov: {
-                  heading: heading,
-                  pitch: 30
-                }
-              };
-            var panorama = new google.maps.StreetViewPanorama(
-              document.getElementById('pano'), panoramaOptions);
-          } else {
-            infowindow.setContent('<div>' + marker.title + '</div>' +
-              '<div>No Street View Found</div>');
-          }
+    self.filterMarkers = ko.computed(function () {
+        // set marker and extend bounds (showListings)
+        if(self.visible() === true) {
+            self.marker.setMap(map);
+            bounds.extend(self.marker.position);
+            map.fitBounds(bounds);
+        } else {
+            self.marker.setMap(null);
         }
+    });
 
-        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    this.marker.addListener('click', function() {
+        this.setIcon(highlightedIcon);
+        populateInfoWindow(this, self.street, self.city, self.phone, infoWindow);
+        toggleBounce(this);
+        map.panTo(this.getPosition());
+    });
 
-        infowindow.open(map, marker);
-      }
+    this.show = function(location) {
+        google.maps.event.trigger(self.marker, 'click');
+    };
 
-    function showListings() {
-      var bounds = new google.maps.LatLngBounds();
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-        bounds.extend(markers[i].position);
-      }
-      map.fitBounds(bounds);
-    }
-
-    function hideListings() {
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-      }
-    }
-  }
-
-  function makeMarkerIcon() {
-    var markerImage = new google.maps.MarkerImage(
-      'http://pedroperone.com/normal.png',
-      new google.maps.Size(30, 30),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(30,30));
-    return markerImage;
-  }
-
-
-  function makeMarkerIconHighlight() {
-    var markerImage = new google.maps.MarkerImage(
-      'http://pedroperone.com/highlight.png',
-      new google.maps.Size(30, 30),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(30,30));
-    return markerImage;
-  }
-};
-
-var Location = function(data) {
-	var self = this;
-	this.title = data.title;
-	this.lat = data.location.lat;
-	this.long = data.location.long;
-
-	this.visible = ko.observable(true);
-
-	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
-
-	this.showMarker = ko.computed(function() {
-		if(this.visible() === true) {
-			this.marker.setMap(map);
-		} else {
-			this.marker.setMap(null);
-		}
-		return true;
-	}, this);
-
-  this.pop = function(place) {
+    // creates bounce effect when item selected
+    this.bounce = function(place) {
 		google.maps.event.trigger(self.marker, 'click');
 	};
 
 };
 
-ko.applyBindings(new AppViewModel());
+/* View Model */
+var ViewModel = function() {
+    var self = this;
+
+    this.searchItem = ko.observable('');
+
+    this.mapList = ko.observableArray([]);
+
+    // add location markers for each location
+    locations.forEach(function(location) {
+        self.mapList.push( new LocationMarker(location) );
+    });
+
+    // locations viewed on map
+    this.locationList = ko.computed(function() {
+        var searchFilter = self.searchItem().toLowerCase();
+        if (searchFilter) {
+            return ko.utils.arrayFilter(self.mapList(), function(location) {
+                var str = location.title.toLowerCase();
+                var result = str.includes(searchFilter);
+                location.visible(result);
+				return result;
+			});
+        }
+        self.mapList().forEach(function(location) {
+            location.visible(true);
+        });
+        return self.mapList();
+    }, self);
+};
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, street, city, phone, infowindow) {
+
+    var defaultIcon = makeMarkerIcon();
+
+    if (infowindow.marker != marker) {
+        // Clear the infowindow content to give the streetview time to load.
+        infowindow.setContent('');
+        infowindow.marker = marker;
+
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            marker.setIcon(defaultIcon);
+            infowindow.marker = null;
+        });
+
+        var streetViewService = new google.maps.StreetViewService();
+        var radius = 50;
+
+        var windowContent = '<h4>' + marker.title + '</h4>' +
+            '<p>' + street + "<br>" + city + '<br>' + phone + "</p>";
+
+        // In case the status is OK, which means the pano was found, compute the
+        // position of the streetview image, then calculate the heading, then get a
+        // panorama from that and set the options
+        var getStreetView = function (data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+                var nearStreetViewLocation = data.location.latLng;
+                var heading = google.maps.geometry.spherical.computeHeading(
+                    nearStreetViewLocation, marker.position);
+                infowindow.setContent(windowContent + '<div id="pano"></div>');
+                var panoramaOptions = {
+                    position: nearStreetViewLocation,
+                    pov: {
+                        heading: heading,
+                        pitch: 20
+                    }
+                };
+                var panorama = new google.maps.StreetViewPanorama(
+                    document.getElementById('pano'), panoramaOptions);
+            } else {
+                infowindow.setContent(windowContent + '<div style="color: red">No Street View Found</div>');
+            }
+        };
+        // Use streetview service to get the closest streetview image within
+        // 50 meters of the markers position
+        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+        // Open the infowindow on the correct marker.
+        infowindow.open(map, marker);
+    }
+}
+
+function toggleBounce(marker) {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+        marker.setAnimation(null);
+    }, 1400);
+  }
+}
+
+function makeMarkerIcon() {
+  var markerImage = new google.maps.MarkerImage(
+    'http://pedroperone.com/normal.png',
+    new google.maps.Size(30, 30),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(30,30));
+  return markerImage;
+}
+
+function makeMarkerIconHighlight() {
+  var markerImage = new google.maps.MarkerImage(
+    'http://pedroperone.com/highlight.png',
+    new google.maps.Size(30, 30),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(30,30));
+  return markerImage;
+}
